b4acb926bd678abea9070aaf720e853e9184f4ef
Hanru Vorster <hanru.vorster@gmail.com>
2024-08-02 10:14:36 +0200

    Added ternary to check if it is viable to drink an energy drink bewteen 7:00 and 24:00

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ let cansLeft = 3; // Any number of cans
 const stockCheck = cansLeft < 5 ? `Time to restock!` : `We're stocked!`;
 console.log(stockCheck);
 
-
 // #### Challenge 3: Workout Intensity
 // Based on the current heart rate, determine if the user should drink a Monster Energy drink to boost their workout. 
 // If the heart rate is below 100 bpm, print "Boost needed!", otherwise, print "Energy levels are high!".
@@ -24,7 +23,6 @@ console.log(stockCheck);
 let heartRate = 99; // Current heart rate in bpm
 const drinkEnerygy = heartRate < 100 ? `Boost needed!` : `Energy levels are high!`;
 console.log(drinkEnerygy);
-
 
 // #### Challenge 4: Temperature Suitability
 // Monster Energy drinks are best served cold. Given the current temperature, use a ternary to print "Chilled to perfection!" 
@@ -34,23 +32,18 @@ let currentTemp = 4; // Current temperature in °C
 const TempCheck = currentTemp <= 5 ? `Chilled to perfection!` : `Needs a cooler!`;
 console.log(TempCheck);
 
-
 // #### Challenge 5: Late Night Coding Session
 // Determine if it's a good idea to have a Monster Energy drink based on the current hour.
 // If it's between 7am and midnight (24-hour format), print "Unleash the beast!",
 // otherwise, print "Better stick to water."
-
 // Define the current hour in 24-hour format
-let currentHour = 22; 
+let currentHour = 1; 
 
 // Write your code below this line to determine if it's a good idea to have a Monster Energy drink based on the current hour
 // If it's between 7am and midnight (24-hour format), print "Unleash the beast!"
 // Otherwise, print "Better stick to water."
-
-
-
-
-
+const lateEnergyDrink = currentHour >= 7 && currentHour <= 24 ? `Unleash the beast!` : `Better stick to water.`
+console.log(lateEnergyDrink);
 
 // In each of these challenges, the ternary operator is used to decide between two options based on a condition, 
 // demonstrating its utility for concise conditional logic. Remember that while ternaries can make the code more succinct, 
